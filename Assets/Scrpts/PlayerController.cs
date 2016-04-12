1a3247d3e49a0646e282678c21173f3f5ec47bf9
imp908@ <imp908@yandex.ru>
2016-04-12 22:27:55 +0300

    Rollerball learning example

---
+++ Assets/Scrpts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Vector3 moveForce;
+	public GUIText countText;
+
+	private int count;
+
+	void Start()
+	{
+		count = 0;
+		TextRefresh();
+	}
+
+
+	
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		moveForce = new Vector3 (moveHorizontal * speed , 0 , moveVertical * speed);
+		rigidbody.AddForce (moveForce);
+
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("trigered");
+		if(other.gameObject.tag == "Pickup"){
+			Debug.Log ("not ground tag");
+			other.gameObject.SetActive(false);
+			count=count + 1 ;
+			TextRefresh();
+		}
+	}
+
+	void TextRefresh()
+	{
+		countText.text = "Score: " + count.ToString ();
+	}
+}
---
+++ Assets/Scrpts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public float rotationSpeed;
+	
+	void Update()
+	{
+		transform.Rotate(0.0F, rotationSpeed , 0.0F, Space.World);
+	}
+}
